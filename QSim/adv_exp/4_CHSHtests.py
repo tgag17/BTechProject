acd78762970626db191ef474b83d463bdd367d71
Tanya Garg <62295887+tgag17@users.noreply.github.com>
2022-05-04 15:48:35 +0530

    Add files via upload

--- QSim/adv_exp/4_CHSHtests.py
+++ QSim/adv_exp/4_CHSHtests.py
@@ -1,79 +1,51 @@
+from qiskit import QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit, execute, BasicAer
 import numpy as np
-
-from qiskit import QuantumCircuit, Aer
 import time
 
-sim = Aer.get_backend('aer_simulator')
+#Options & Noise goes here - Don't change options variable name & block
+options = {
+"rotation_error": {'rx':[1.0, 0.0], 'ry':[1.0, 0.0], 'rz':[1.0,0.0]},
+"tsp_model_error": [1.0, 0.0],
+"thermal_factor": 1.0,
+"decoherence_factor": 1.0,
+"depolarization_factor": 1.0,
+"bell_depolarization_factor": 1.0,
+"decay_factor": 1.0,
+}
 
-def make_chsh_circuit():
-   
-    chsh_circuits = []
-    theta = np.pi/10
+backend = BasicAer.get_backend('dm_simulator')
 
-    obs_vec = ['00', '01', '10', '11']
-    for el in obs_vec:
-        qc = QuantumCircuit(2,2)
-        qc.h(0)
-        qc.cx(0, 1)
-        qc.barrier() 
-        qc.ry(theta, 0)
-        qc.barrier() 
-        for a in range(2):
-            if el[a] == '1':
-                qc.h(a) 
-        qc.measure(range(2),range(2))
-        chsh_circuits.append(qc)
+s = 0
+for i in range(4):
+  qc = QuantumCircuit()
+  q = QuantumRegister(2, 'q')
+  c = ClassicalRegister(2, 'c')
 
-    return chsh_circuits  
+  qc.add_register(q)
+  qc.add_register(c)
 
+  a = np.pi/10
 
-def compute_chsh_witness(counts):
-    
-    CHSH1 = []
-    CHSH2 = []
-    
-    for i in range(0, len(counts), 4):  
-        theta_dict = counts[i:i + 4]
-        zz = theta_dict[0]
-        zx = theta_dict[1]
-        xz = theta_dict[2]
-        xx = theta_dict[3]
+  qc.h(0)
+  qc.cx(0, 1)
+  qc.ry(a, 0)
+  
+  if i == 1 or i == 3:
+    qc.h(0)
+  if i == 2 or i == 3:
+    qc.h(1)
 
-        no_shots = sum(xx[y] for y in xx)
+  qc.measure(q[0],c[0])
+  qc.measure(q[1],c[1])
 
-        chsh1 = 0
-        chsh2 = 0
+##### 
 
-        for element in zz:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1+= parity*zz[element]
-            chsh2+= parity*zz[element]
-
-        for element in zx:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1+= parity*zx[element]
-            chsh2-= parity*zx[element]
-
-        for element in xz:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1-= parity*xz[element]
-            chsh2+= parity*xz[element]
-
-        for element in xx:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1+= parity*xx[element]
-            chsh2+= parity*xx[element]
-
-        CHSH1.append(chsh1/no_shots)
-        CHSH2.append(chsh2/no_shots)
-    
-    return CHSH1, CHSH2
-
-
-my_chsh_circuits = make_chsh_circuit()
-
-tic = time.time()
-result_ideal = sim.run(my_chsh_circuits).result()
-toc = time.time()
-print(toc-tic)
-CHSH1_ideal, CHSH2_ideal = compute_chsh_witness(result_ideal.get_counts())
+  tic = time.time()
+  job = execute(qc, backend=backend, **options)
+  toc = time.time()
+  job_result = job.result()
+  
+  s += toc-tic
+  
+print(s)
--- QSim/adv_exp/CHSHtest.py
+++ QSim/adv_exp/CHSHtest.py
@@ -1,81 +1,51 @@
+from qiskit import QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit, execute, BasicAer
 import numpy as np
-
-from qiskit import QuantumCircuit, Aer
 import time
 
-sim = Aer.get_backend('aer_simulator')
+#Options & Noise goes here - Don't change options variable name & block
+options = {
+"rotation_error": {'rx':[1.0, 0.0], 'ry':[1.0, 0.0], 'rz':[1.0,0.0]},
+"tsp_model_error": [1.0, 0.0],
+"thermal_factor": 1.0,
+"decoherence_factor": 1.0,
+"depolarization_factor": 1.0,
+"bell_depolarization_factor": 1.0,
+"decay_factor": 1.0,
+}
 
-def make_chsh_circuit(theta_vec, flip_err_x = 0, flip_err_z = 0):
-   
-    chsh_circuits = []
-    
-    for theta in theta_vec:
-        obs_vec = ['00', '01', '10', '11']
-        for el in obs_vec:
-            qc = QuantumCircuit(2,2)
+backend = BasicAer.get_backend('dm_simulator')
+arr = np.linspace(0, 2*np.pi, 21)
+
+s = 0
+for a in arr:
+    for i in range(4):
+        qc = QuantumCircuit()
+        q = QuantumRegister(2, 'q')
+        c = ClassicalRegister(2, 'c')
+
+        qc.add_register(q)
+        qc.add_register(c)
+
+        qc.h(0)
+        qc.cx(0, 1)
+        qc.ry(a, 0)
+        
+        if i == 1 or i == 3:
             qc.h(0)
-            qc.cx(0, 1)
-            qc.barrier() 
-            qc.ry(theta, 0)
-            qc.barrier() 
-            for a in range(2):
-                if el[a] == '1':
-                    qc.h(a) 
-            qc.measure(range(2),range(2))
-            chsh_circuits.append(qc)
+        if i == 2 or i == 3:
+            qc.h(1)
 
-    return chsh_circuits  
+        qc.measure(q[0],c[0])
+        qc.measure(q[1],c[1])
 
+        ###########################
 
-def compute_chsh_witness(counts):
+        tic = time.time()
+        job = execute(qc, backend=backend, **options)
+        toc = time.time()
+        job_result = job.result()
+        
+        s += toc-tic
     
-    CHSH1 = []
-    CHSH2 = []
-    
-    for i in range(0, len(counts), 4):  
-        theta_dict = counts[i:i + 4]
-        zz = theta_dict[0]
-        zx = theta_dict[1]
-        xz = theta_dict[2]
-        xx = theta_dict[3]
-
-        no_shots = sum(xx[y] for y in xx)
-
-        chsh1 = 0
-        chsh2 = 0
-
-        for element in zz:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1+= parity*zz[element]
-            chsh2+= parity*zz[element]
-
-        for element in zx:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1+= parity*zx[element]
-            chsh2-= parity*zx[element]
-
-        for element in xz:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1-= parity*xz[element]
-            chsh2+= parity*xz[element]
-
-        for element in xx:
-            parity = (-1)**(int(element[0])+int(element[1]))
-            chsh1+= parity*xx[element]
-            chsh2+= parity*xx[element]
-
-        CHSH1.append(chsh1/no_shots)
-        CHSH2.append(chsh2/no_shots)
-    
-    return CHSH1, CHSH2
-
-
-number_of_thetas = 21
-theta_vec = np.linspace(0,2*np.pi,number_of_thetas)
-my_chsh_circuits = make_chsh_circuit(theta_vec, 0)
-
-tic = time.time()
-result_ideal = sim.run(my_chsh_circuits).result()
-toc = time.time()
-print(toc-tic)
-CHSH1_ideal, CHSH2_ideal = compute_chsh_witness(result_ideal.get_counts())
+print(s)
